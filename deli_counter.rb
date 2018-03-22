e23e26ed0b77474e89d7dcba0557580143c5fc9a
Benjamin Bowes <bbowes999@gmail.com>
2018-03-22 14:46:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ end
 
 def take_a_number(katz_deli, name)
    katz_deli.push(name)
-"Welcome, #{name}. You are number #{katz_deli.length} in line."
+puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
  end
 
 
